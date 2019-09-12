47e39f9863b5eebe5c0991683569d8604531a9bf
hudaokeji <github@hudaokeji.com>
2019-09-13 00:16:02 +0900

    moved from skylark-utils-dom

---
+++ dist/skylark-domx-eventer/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-domx-eventer - The skylark eventer library for dom api extension.
+ * @author Hudaokeji Co.,Ltd
+ * @version v0.9.0
+ * @link www.skylarkjs.org
+ * @license MIT
+ */
+define(["./eventer"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ src/main.js
@@ -0,0 +1,5 @@
+define([
+	"./eventer"
+],function(eventer){
+	return eventer;
+});
